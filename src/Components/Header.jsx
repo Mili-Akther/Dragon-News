6c6d673a3642ac509477ec9f728617adffddfe02
Mili Akther <miliakthermilu@gmail.com>
2025-03-20 21:29:22 +0600

    Implement React Fast Marquee and Moment Js on Header

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import logo from "../assets/logo.png"
+import moment from 'moment';
 const Header = () => {
     return (
       <div className="flex flex-col justify-center items-center gap-2 py-2">
@@ -7,6 +8,7 @@ const Header = () => {
           <img className="w-[300px]" src={logo} alt="" />
         </div>
         <h2 className="text-gray-400">Journalism Without Fear or Favour</h2>
+        <p>{moment().format("dddd, MMMM Do YYYY ")}</p>
       </div>
     );
 };
