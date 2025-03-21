b2838d7579c1763eda74a0d4a2a4c663d94f88cc
Mili Akther <miliakthermilu@gmail.com>
2025-03-21 06:42:50 +0600

    Design LeftNavbar and Fetch and Show Category Data

--- src/Layouts/HomeLayout.jsx
+++ src/Layouts/HomeLayout.jsx
@@ -1,5 +1,6 @@
 import Header from "../Components/Header";
 import LatestNews from "../Components/LatestNews";
+import LeftNavbar from "../Components/layout-component/LeftNavbar";
 import Navbar from "../Components/Navbar";
 
 const HomeLayout = () => {
@@ -15,7 +16,7 @@ const HomeLayout = () => {
           <Navbar></Navbar>
         </nav>
         <main className="w-11/12 mx-auto pt-5 grid md:grid-cols-12 gap-3">
-        <aside className="left col-span-3">left navbar</aside>
+        <aside className="left col-span-3"><LeftNavbar></LeftNavbar></aside>
         <section className="col-span-6">main content</section>
         <aside className=" col-span-3">Right navbar</aside>
         </main>
